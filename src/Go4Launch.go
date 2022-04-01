d779b77ca378851e5747bf2a88706092c51040b8
Mille Codex <39792005+millecodex@users.noreply.github.com>
2022-04-01 08:42:00 +0800

    Create Go4Launch.go

---
+++ src/Go4Launch.go
@@ -0,0 +1,13 @@
+//main go file
+package main
+
+import (
+	"fmt"
+)
+
+// looks like main gets autorun
+func main() {
+	fmt.Println("Go for launch")
+  fmt.Println("Roger that, we are go for launch")
+}
+
