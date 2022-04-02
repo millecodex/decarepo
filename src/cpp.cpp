bcc1eecfb1872e7b16e47d3ff01cfde0896906b3
Mille Codex <39792005+millecodex@users.noreply.github.com>
2022-04-03 01:14:21 +1300

    Create cpp.cpp

---
+++ src/cpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// remember make files?
+using namespace std;
+int main() {
+  cout << "Hello World!\n";
+
+// still my favourite loop syntax
+  for (int i=0; i<10; i++){
+    cout << "the variable i="<< i<< '\n';
+  }
+}
